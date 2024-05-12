677bce51a7c61aedcfd4c3df527ce587621bbc70
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-05-12 13:38:11 +0200

    Update app.js, added operate function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Operations functions
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +14,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// Operation variables (hardcoded for now)
+let num1 = 5;
+let num2 = 3;
+let operand = '+';
+
+// return a result by calling it's required operation function
+function operate(num1, num2, operand) {
+    switch (operand) {
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate(num1, num2, operand));
